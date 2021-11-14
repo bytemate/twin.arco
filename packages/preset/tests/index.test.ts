0d864116c7514ac58a2b26aa93e1860b1e3fb34a
Hanxx <xsytby1112@gmail.com>
2021-11-14 19:14:26 +0800

    test(preset): add unit test for tailwind

--- packages/preset/tests/index.test.ts
+++ packages/preset/tests/index.test.ts
@@ -1,4 +1,6 @@
 import { Processor } from 'windicss/lib/index';
+import postcss from 'postcss';
+import tailwindcss from 'tailwindcss';
 import preset from '@/index';
 
 describe('Twin.arco preset', () => {
@@ -11,5 +13,28 @@ describe('Twin.arco preset', () => {
     );
     expect(result.ignored.length).toBe(0);
     expect(result.styleSheet.build()).toMatchSnapshot();
+  });
+
+  test('TailwindCSS preset', async () => {
+    const processor = postcss([
+      tailwindcss({
+        presets: [preset()],
+        theme: {},
+        darkMode: 'class',
+        content: [
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-expect-error
+          {
+            raw: '<div class="text-red-1 bg-red-1 border-red-1 text-arco-text-1 bg-arco-bg-1 border-arco-border-1"></div>',
+          },
+        ],
+      }),
+    ]);
+
+    const result = await processor.process(String.raw`@tailwind utilities;`, {
+      from: undefined,
+    });
+
+    expect(result.css).toMatchSnapshot();
   });
 });
